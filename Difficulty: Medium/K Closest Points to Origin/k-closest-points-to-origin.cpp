7dab3c7fc137e38853baa6300441de2eeb63e23c
anshikabansal18 <91780173+anshikabansal18@users.noreply.github.com>
2025-10-23 12:42:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // code here
+        priority_queue<pair<int,pair<int,int>>> pq;
+        vector<vector<int>> res;
+        int n=points.size();
+        for(int i=0;i<n && i<k;i++){
+           int value = points[i][0]*points[i][0]+points[i][1]*points[i][1];
+           pq.push({value,{points[i][0],points[i][1]}});
+        }
+        for(int i=k;i<n;i++){
+             int value = points[i][0]*points[i][0]+points[i][1]*points[i][1];
+                 pq.push({value,{points[i][0],points[i][1]}});
+                 pq.pop();
+        }
+         n=pq.size();
+        for(int i=0;i<n;i++){
+            res.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
